d2181f4029b961a2ed067e58f3a4d39b9c64a87c
nepovezloo <89730655+nepovezloo@users.noreply.github.com>
2021-09-27 22:16:09 +0300

    Code optimtzation

--- Practice_cursor/variables_and_types.js
+++ Practice_cursor/variables_and_types.js
@@ -1,33 +1,22 @@
 const apples= 15.678;
 const avokado= 90.2345;
 const pineapple= 123.965;
-const MaxPrice= Math.max(apples, avokado, pineapple)
-const MinPrice= Math.min(apples, avokado, pineapple)
+const MaxPrice= Math.max(apples, avokado, pineapple);
+const MinPrice= Math.min(apples, avokado, pineapple);
 console.log(MaxPrice);
 console.log(MinPrice);
 const sum= apples+avokado+pineapple;
 console.log(sum);
-const applesFloor= Math.floor(apples);
-const avokadoFloor= Math.floor(avokado);
-const pineappleFloor= Math.floor(pineapple);
-const sumFloor= applesFloor+avokadoFloor+pineappleFloor;
+const sumFloor= Math.floor(apples)+Math.floor(avokado)+Math.floor(pineapple);
 console.log(sumFloor);
 const sumFloorTo100= Math.floor(sumFloor/100)*100;
 console.log(sumFloorTo100);
-function boooleanSum1(sum1){
-    if (sumFloor % 2 == 0)
-      return true;
-    else
-      return false;
-}
-console.log(boooleanSum1(sumFloor));
+const evenNumber= sumFloor % 2 === 0;
+console.log(evenNumber);
 const cash= 500;
 const change= cash-sum;
 console.log(change);
-const applesFloor2 = Math.floor(apples*100)/100;
-const avokadoFloor2 = Math.floor(avokado*100)/100;
-const pineappleFloor2 = Math.floor(pineapple*100)/100;
-const sumFloor2= applesFloor2+avokadoFloor2+pineappleFloor2;
+const sumFloor2= Math.floor(apples*100)/100 +Math.floor(avokado*100)/100+Math.floor(pineapple*100)/100;
 const avarage= sumFloor2/3;
 console.log(avarage);
 const randomDiscount = Math.random();
@@ -36,14 +25,13 @@ const totalSum= Math.floor((sumFloor*totalDiscount)*100)/100;
 console.log(totalSum);
 const clearProfitAllProducts= (sumFloor/2)-totalSum;
 console.log(clearProfitAllProducts);
-const AllInOne= `Максимальна ціна : ${MaxPrice} 
+console.log(`Максимальна ціна : ${MaxPrice} 
 Мінімальна ціна : ${MinPrice} 
 Вартість всіх товарів : ${sum} 
 Сума товарів при округленні до цілих чисел : ${sumFloor} 
 Сума товарів округлена до сотень : ${sumFloorTo100} 
-Округлена сума є парним числом : ${boooleanSum1(sumFloor)} 
+Округлена сума є парним числом :  ${evenNumber}
 Решта при оплаті клієнтом 500 гривнями : ${change} 
 Середнє значення цін : ${avarage} 
 Ціна зі знижкою : ${totalSum} 
-Чистий прибуток : ${clearProfitAllProducts}`;
-console.log(AllInOne);
+Чистий прибуток : ${clearProfitAllProducts}`);
