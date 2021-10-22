cba707aac14165b49ee31adc1e4e4071e89b5074
nepovezloo <89730655+nepovezloo@users.noreply.github.com>
2021-10-22 17:13:31 +0300

    Optimization

--- HW8/classes.js
+++ HW8/classes.js
@@ -16,24 +16,40 @@ class Student {
        this.marks.push(mark);
  }
    getAvarageMark() {
-    return this.marks.reduce(sum, arg => sum + arg, 0) / this.marks.length;
+    return this.marks.reduce((sum, arg) => sum + arg, 0) / this.marks.length;
  }
    dismiss() {
     this.dismis = this.marks;
-    this.dismis = null;
+    this.marks = null;
    }
    recover() {
-       this.dismis = this.marks;
+       this.marks = this.dismis;
    }
 }
 
-student1 = new Student('Культурное заведение "Zabegailovka"', '1 - й столик', 'Васыль Нигадяев' )
-
+student1 = new Student(' Харківський національний університет радіоелектроніки ', ' 3 курс ', ' Василь Негідник ' )
+console.log(`Інформація про студента: ${student1.getInfo()}`);
+console.log(`Оцінки студента: ${student1.getMarks}`);
+student1.setMark = 5;
+console.log(`Оцінки студениа після доставленої оцінки: ${student1.getMarks}`);
+console.log(`Cередній бал студента: ${student1.getAvarageMark()}`)
+student1.dismiss();
+console.log(`Після виключення у студента нема оцінок: ${student1.getMarks}`);
+student1.recover()
+console.log(`Після поновлення студента повертаються всі оцінки які були : ${student1.getMarks}`);
 //Advanced
  class BudgetStudent extends Student{
-     constructor(university, course, fullName)
+     constructor(university, course, fullName){
        super(university, course, fullName)
-   getSchoolarShip() {
-       return console.log(`Ви отримали стипендію`)
-   }
+       setInterval(() => this.getSchoolarShip() , 30000);
+    }
+     getSchoolarShip() {
+        if(this.getAvarageMark() >= 4.0){
+        return `Ви отримали 1400 грн. стипендії`
+        }
+     }
+  
  }
+ studentWithScoolarShip = new BudgetStudent(' Харківський національний університет радіоелектроніки ', ' 2 курс ', ' Федір Ленькович')
+ console.log(studentWithScoolarShip.getInfo());
+ console.log(`Як бачимо, студент отриимує стипендію: ${studentWithScoolarShip.getSchoolarShip()}`)
