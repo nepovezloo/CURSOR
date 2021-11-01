68ea7de1082ecb999d74a851aa3e4ffec5ae106f
nepovezloo <89730655+nepovezloo@users.noreply.github.com>
2021-11-01 20:29:18 +0200

    Optimization

--- HW9/dom.js
+++ HW9/dom.js
@@ -4,16 +4,16 @@ document.body.append(bigCube);
 
 const generateBlocks = () => {
 
-    function randomSmallCubeColor() {
-        const randomColor = () => Math.floor(Math.random() * 256);
-        return 'rgb(' + randomColor() + ',' + randomColor() + ',' + randomColor() + ')';
-    }
+  function getRandomRGB() {
+    const randomColor = () => Math.floor(Math.random() * 256);
+    return `rgb(${randomColor()},${randomColor()},${randomColor()})`;
+  }
 
   for (let h = 0; h < 5; h++) {
       for (let w = 0; w < 5; w++){
     const smallCube = document.createElement('div');
     smallCube.classList.add('box');
-    smallCube.style.backgroundColor = randomSmallCubeColor();
+    smallCube.style.backgroundColor = getRandomRGB();
     bigCube.append(smallCube);
   }
  }
@@ -24,7 +24,7 @@ generateBlocks();
 const generateBlocksInterval = () => {
     setInterval(()=> {
       bigCube.innerHTML = '' 
-        generateBlocks()
+      generateBlocks()
     },1000)
   }
   generateBlocksInterval();
