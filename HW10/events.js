d00fde8c0015bfe418385716e94cf66ad4415910
nepovezloo <89730655+nepovezloo@users.noreply.github.com>
2021-11-04 21:23:01 +0200

    Optimization

--- HW10/events.js
+++ HW10/events.js
@@ -1,1 +1,18 @@
-console.log('hello')
+function playSound (e) {
+    let audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    let key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+    key.classList.add('playing')
+    if(!audio) return;
+    audio.currentTime = 0;
+        audio.play();
+
+   }
+   function removeTransition (e) {
+     if(e.propertyName !== 'transform') return;
+     console.log(e.propertyName)
+     this.classList.remove('playing')
+   }
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+ window.addEventListener('keydown' , playSound);
